e1c2e869a8544ae98c483faddebbe14245483ba2
taodi <mlzgg@sina.cn>
2020-11-23 15:27:23 +0800

    添加icon,添加阻止esc键脚本

--- FuckESC Extension/script.js
+++ FuckESC Extension/script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+    (function (window) {
+        const KEYCODE_ESCAPE = 27;
+
+        window.addEventListener("keypress", function suppressEscape(event) {
+            if (event.keyCode === KEYCODE_ESCAPE) {
+                event.preventDefault();
+                console.log("FuckESC")
+            }
+        });
+    })(window);
 });
